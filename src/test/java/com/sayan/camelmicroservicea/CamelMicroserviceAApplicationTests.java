76aeb48605656dfb3a2e39890c7d22ae9a1a68ac
Cadris <cadrissilver@gmail.com>
2022-01-28 00:47:49 +0530

    Innitial Commit

---
+++ src/test/java/com/sayan/camelmicroservicea/CamelMicroserviceAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayan.camelmicroservicea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
